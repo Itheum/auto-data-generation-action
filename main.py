b15364e9bb71f38bd1c514fc0842d68093bfbcf4
Damian <damian.ovidiu27@gmail.com>
2023-05-05 15:18:51 +0300

    simplify

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
 
-test_json = {"test2": "test2"}
+object = {"key": "value"}
 
-output_file = open("output/test_file.json", "w")
+output_file = open("output/data.json", "w")
 output_file.truncate(0)
-output_file.write(json.dumps(test_json, sort_keys=True, indent=4))
+output_file.write(json.dumps(object))
 output_file.close()
